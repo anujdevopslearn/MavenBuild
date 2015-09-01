f30c30c2ca60a0ac5d575b61442251013f97c188
Steven Haines <steven.haines@disney.com>
2015-09-01 10:32:58 -0400

    Initial commit

---
+++ src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
@@ -0,0 +1,14 @@
+package com.geekcap.vmturbo;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class HelloWorldServlet extends HttpServlet 
+{
+  public void service( HttpServletRequest req, HttpServletResponse res ) throws IOException {
+    PrintWriter out = res.getWriter();
+    out.println( "Hello, World!" );
+    out.close();
+  }
+}
