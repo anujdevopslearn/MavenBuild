a3da5d8d82d11475e5e1fc899871f1f7a975ed07
ligado <steve@geekcap.com>
2016-07-24 13:04:06 -0400

    Missed a couple files

--- src/test/java/com/geekcap/vmturbo/ThingTest.java
+++ src/test/java/com/geekcap/vmturbo/ThingTest.java
@@ -1,7 +1,17 @@
 package com.geekcap.vmturbo;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by shaines on 7/24/16.
  */
 public class ThingTest {
+
+    @Test
+    public void testN() {
+        Thing t = new Thing();
+        t.setN(5);
+        Assert.assertEquals("N should be 5", 5, t.getN());
+    }
 }
