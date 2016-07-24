361eeec5473425dd3ef8fdbd4240d9b1d8acd71e
ligado <steve@geekcap.com>
2016-07-24 13:03:35 -0400

    Adding a single unit test

---
+++ src/main/java/com/geekcap/vmturbo/Thing.java
@@ -0,0 +1,13 @@
+package com.geekcap.vmturbo;
+
+public class Thing {
+  private int n = 0;
+
+  public int getN() {
+    return n;
+  }
+
+  public void setN(int n) {
+    this.n = n;
+  }
+}
---
+++ src/test/java/com/geekcap/vmturbo/ThingTest.java
@@ -0,0 +1,7 @@
+package com.geekcap.vmturbo;
+
+/**
+ * Created by shaines on 7/24/16.
+ */
+public class ThingTest {
+}
