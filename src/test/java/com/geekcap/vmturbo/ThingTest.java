6f85d849363cc137208d3efdfdc7a67e9dbcdf33
Anuj Sharma <anuj_sharma401@yahoo.com>
2020-01-05 10:38:41 +0530

    Update ThingTest.java

--- src/test/java/com/geekcap/vmturbo/ThingTest.java
+++ src/test/java/com/geekcap/vmturbo/ThingTest.java
@@ -11,7 +11,7 @@ public class ThingTest {
     @Test
     public void testN() {
         Thing t = new Thing();
-        t.setN(5);
+        t.setN(15);
         Assert.assertEquals("N should be 5", 5, t.getN());
     }
 }
